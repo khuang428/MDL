89a69e1bb7aa53ff014c5c6d0ed01c32b8fe9b51
Karen Huang <khuang428@gmail.com>
2017-05-08 10:53:56 -0400

    almost

--- my_main.c
+++ my_main.c
@@ -14,19 +14,19 @@
   pop: remove the top matrix on the origin stack
 
   move/scale/rotate: create a transformation matrix 
-                     based on the provided values, then 
-		     multiply the current top of the
-		     origins stack by it.
+  based on the provided values, then 
+  multiply the current top of the
+  origins stack by it.
 
   box/sphere/torus: create a solid object based on the
-                    provided values. Store that in a 
-		    temporary matrix, multiply it by the
-		    current top of the origins stack, then
-		    call draw_polygons.
+  provided values. Store that in a 
+  temporary matrix, multiply it by the
+  current top of the origins stack, then
+  call draw_polygons.
 
   line: create a line based on the provided values. Store 
-        that in a temporary matrix, multiply it by the
-	current top of the origins stack, then call draw_lines.
+  that in a temporary matrix, multiply it by the
+  current top of the origins stack, then call draw_lines.
 
   save: call save_extension with the provided filename
 
@@ -52,6 +52,8 @@
 void my_main() {
 
   int i;
+  double step = 0.1;
+  double theta;
   struct matrix *tmp;
   struct stack *s;
   screen t;
@@ -65,59 +67,57 @@ void my_main() {
     switch (op[i].opcode) {
     case PUSH :
       push(s);
-      break;
     case POP :
       pop(s);
-      break;
     case MOVE :
-
+      tmp = make_translate(op[i].op.move.d[0],op[i].op.move.d[1],op[i].op.move.d[2]);
       matrix_mult(peek(s), tmp);
       copy_matrix(tmp, peek(s));
       tmp->lastcol = 0;
-      break;
     case ROTATE :
-
+      theta = op[i].op.rotate.degrees;
+      theta *= (M_PI / 180);
+      if(op[i].op.rotate.axis == 0){
+	tmp = make_rotX(theta);
+      }else if(op[i].op.rotate.axis == 1){
+	tmp = make_rotY(theta);
+      }else if(op[i].op.rotate.axis == 2){
+	tmp = make_rotZ(theta);
+      }
       matrix_mult(peek(s), tmp);
       copy_matrix(tmp, peek(s));
       tmp->lastcol = 0;
-      break;
     case SCALE :
-
+      tmp = make_scale(op[i].op.scale.d[0],op[i].op.scale.d[1],op[i].op.scale.d[2]);
       matrix_mult(peek(s), tmp);
       copy_matrix(tmp, peek(s));
       tmp->lastcol = 0;
-      break;
     case BOX :
-
+      add_box(tmp, op[i].op.box.d0[0], op[i].op.box.d0[1], op[i].op.box.d0[2],
+	      op[i].op.box.d1[0], op[i].op.box.d1[1], op[i].op.box.d1[2]);
       matrix_mult(peek(s),tmp);
       draw_polygons(tmp,t,g);
       tmp->lastcol = 0;
-      break;
     case SPHERE :
-
+      add_sphere(tmp, op[i].op.sphere.d[0], op[i].op.sphere.d[1], op[i].op.sphere.d[2],op[i].op.sphere.r, step);
       matrix_mult(peek(s),tmp);
       draw_polygons(tmp,t,g);
       tmp->lastcol = 0;
       break;
     case TORUS :
-
+      add_torus(tmp,op[i].op.torus.d[0],op[i].op.torus.d[1],op[i].op.torus.d[2], op[i].op.torus.r0,op[i].op.torus.r1, step);
       matrix_mult(peek(s),tmp);
       draw_polygons(tmp,t,g);
       tmp->lastcol = 0;
-      break;
     case LINE :
-
+      add_edge(tmp, op[i].op.line.p0[0], op[i].op.line.p0[1], op[i].op.line.p0[2],op[i].op.line.p1[0], op[i].op.line.p1[1], op[i].op.line.p1[2]);      
       matrix_mult(peek(s),tmp);
       draw_lines(tmp,t,g);
       tmp->lastcol = 0;
-      break;
     case SAVE :
-
-      save_extension(t, line);
-      break;
+      save_extension(t,"file");
     case DISPLAY :
       display(t);
-      break;
     }
   }
 }
